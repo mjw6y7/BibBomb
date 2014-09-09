48267f80cd11066d82d2723eef5b06b0619afacc
mark <mjw6y7@mst.edu>
2014-09-08 22:32:18 -0500

    Updated Required Bibtex Fields

--- BibBomp.py
+++ BibBomp.py
@@ -1,13 +1,39 @@
-bibFile = "test.bib"
+bibFile = 'test.bib'
 
-Fields = {"inproceedings":["author","booktitle","pages","publisher","title","year"],
-          "article":["author","journal","number","pages","title","volume","year"],
-          "techreport":["author","institution","title","year"],
-          "incollection":["author","booktitle","pages","publisher","title","year"],
-          "book":["author","publisher","title","year"],
-          "inbook":["author","booktitle","pages","publisher","title","year"],
-          "proceedings":["editor","publisher","title","year"],
-          "phdthesis":["author","school","title","year"],
-          "mastersthesis":["author","school","title","year"],
-          "electronic":["author","title","url","year"],
-          "misc":["author","howpublished","title","year"]}
+Req = 0
+Opt = 1
+Fields = {'article':[['author','title','journal','year'],
+                     ['volume','number','pages','month','note']],
+          'book':[['author','title','publisher','year'],
+                  ['volume','number','series','address','edition','month','note']],
+          'booklet':[['title','year'],
+                     ['author','howpublished','address','month','note']],
+          'conference':[['author','title','booktitle','year'],
+                        ['editor','volume','number','series','pages','address','month','publisher','note']],
+          'inbook':[['author','editor','title','chapter','pages','publisher','year'],
+                    ['volume','number','series','type','address','edition','month','note']],
+          'incollection':[['author','title','booktitle','publisher','year'],
+                          ['editor','volume','number','series','type','chapter','pages','address','edition','month','note']]
+          'inproceedings':[['author','title','booktitle','year'],
+                           ['editor','volume','number','series','pages','address','organization','publisher','month','note']],
+          'manual':[['title','year'],
+                    ['author','organization','address','edition','month','note']],
+          'mastersthesis':[['author','title','school','year'],
+                           ['type','address','month','note']],
+          'misc':[[],
+                  ['author','title','howpublished','year','month','note']],
+          'phdthesis':[['author','title','school','year'],
+                       ['address','month','keywords','note']]
+          'proceedings':[['title','year'],
+                         ['editor','volume','number','series','address','organization','publisher','month','note']],
+          'techreport':[['author','title','institution','year'],
+                        ['type','number','address','month','note']],
+          'unpublished':[['author','title','note'],
+                         ['year','month']],
+          'patent':[[],
+                    []],
+          'collection':[[],
+                        []],
+          'electronic':[[],
+                        []]}
+
