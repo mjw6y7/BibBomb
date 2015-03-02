d3c7ce07aa0a0973d4a1e072c7043c1cf8ae0c33
Michael Wisely <michaelwisely@gmail.com>
2015-03-01 19:56:11 -0600

    Print to stderr

--- BibBomb.py
+++ BibBomb.py
@@ -83,25 +83,25 @@ def main(args):
     for inputfile in args.bib_file:
 
         if not os.path.exists(inputfile):
-            print('Cannot open {}. Skipping...'.format(inputfile))
+            print('Cannot open {}. Skipping...'.format(inputfile), file=sys.stderr)
             continue
 
-        print('Parsing {}'.format(inputfile))
+        print('Parsing {}'.format(inputfile), file=sys.stderr)
         err, opt, year = parse_and_validate(inputfile)
 
         if len(err) == 0:
-            print('{0} contains no errors'.format(inputfile))
+            print('{0} contains no errors'.format(inputfile), file=sys.stderr)
         else:
-            print('{0} contains errors in:{1}'.format(inputfile, err))
+            print('{0} contains errors in:{1}'.format(inputfile, err), file=sys.stderr)
 
         if len(opt) != 0:
-            print('{0} entries are missing optional fields'.format(len(opt)))
+            print('{0} entries are missing optional fields'.format(len(opt)), file=sys.stderr)
 
         if args.fix:
-            print("Cite key repairer isn't ready yet...")
+            print("Cite key repairer isn't ready yet...", file=sys.stderr)
 
         if args.plot:
-            print('Plotting histogram for {}'.format(inputfile))
+            print('Plotting histogram for {}'.format(inputfile), file=sys.stderr)
             plot_years(year)
 
 
